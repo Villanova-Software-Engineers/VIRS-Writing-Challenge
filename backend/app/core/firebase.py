0c884cbb5d27d2beb7f8f30f6d01e7f09f2e745f
JayBradCoding <jbradl24@villanova.edu>
2026-01-28 19:50:34 -0500

    Finished initial set-up

--- backend/app/core/firebase.py
+++ backend/app/core/firebase.py
@@ -1,5 +1,14 @@
+import os
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, auth
+from .config import settings
 
-cred = credentials.Certificate("path/to/serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+if os.path.exists("/etc/secrets/firebase-adminsdk.json"):
+    firebase_creds_path = "/etc/secrets/firebase-adminsdk.json"
+else:
+    firebase_creds_path = settings.FIREBASE_CREDENTIALS
+
+def initialize_firebase():
+    if not firebase_admin._apps:
+        cred = credentials.Certificate(firebase_creds_path)
+        firebase_admin.initialize_app(cred)
