ef3ea65b8318763169c29694637cbe6b729956bb
Nadden Auguste Laventure <nalaventure123@gmail.com>
2026-02-02 17:40:12 -0500

    added model

--- backend/app/schemas/messages.py
+++ backend/app/schemas/messages.py
@@ -4,7 +4,7 @@ class Message(BaseModel):
     author: str
     message_id: str
     content: str
-    timestamp: datetime
+    timestamp: datetime | None = None
     reply_to: Message | None = None
     replies: list[Message] = []
 
